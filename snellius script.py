550f13d94990e88c870aa3b03d2d6c645a579333
giannis3p <johndriopi@gmail.com>
2024-06-14 15:51:51 +0200

    cleanup

--- snellius script.py
+++ snellius script.py
@@ -6,65 +6,99 @@ import csv
 import math
 import matplotlib.pyplot as plt
 from datetime import datetime
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Flatten, Reshape, Dropout
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Flatten, Reshape, Dropout, Conv2D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
+from keras import backend as K
 from keras.constraints import MinMaxNorm
+from keras.callbacks import LearningRateScheduler, EarlyStopping, Callback
+from keras.metrics import RootMeanSquaredError
+from keras.layers import Dropout,  TimeDistributed
+from keras.regularizers import l2
+from keras.initializers import HeNormal
+from tensorflow.keras.layers import Input, Dense, Dropout, LayerNormalization, MultiHeadAttention, Concatenate
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from tensorflow.keras import layers, models, optimizers, losses, metrics
+import cmocean
 
-output_folder = 'C:/Users/Ioannis/Documents/UvA thesis/UvA-thesis/data/all_data'
+# if this doesnt run try float32 also mby deleting prints helps
+tf.keras.backend.set_floatx('float64')
 
-def load_and_drop_zCOM(folder_path):
-    df = pd.concat([pd.read_csv(os.path.join(folder_path, file)) for file in os.listdir(folder_path) if file.endswith('.csv')])
-    df.drop(columns=['zCOM'], inplace=True)
-    return df
+#path
+sorted_concatenated_csv = "C:/Users/Ioannis/Documents/UvA thesis/UvA-thesis/data/test_data_concat/final run concat.csv"
+data = pd.read_csv(sorted_concatenated_csv)
+#data.drop(columns=['zCOM'], inplace=True)
+print(data.head())
 
-dfs = []
-for i in range(1, 9):
-    sim_folder_path = os.path.join(output_folder, f'S{i}')
-    df = load_and_drop_zCOM(sim_folder_path)
-    dfs.append(df)
+#replace mcstep, adjust scale
+data['time'] = (data['mcsteps'] / 10000).astype(int)
+data = data[['time'] + [col for col in data.columns if col != 'time']]
+data.drop(columns=['mcsteps'], inplace=True)
+print(data)
 
-df1, df2, df3, df4, df5, df6, df7, df8 = dfs
+# check value range
+cytokine_columns = ['il8', 'il1', 'il6', 'il10', 'tnf', 'tgf']
+smallest_values = data[cytokine_columns].min()
+largest_values = data[cytokine_columns].max()
 
-df8['time'] = (df8['mcsteps'] / 10000).astype(int)
-df8 = df8[['time'] + [col for col in df8.columns if col != 'time']]
-df8.drop(columns=['mcsteps'], inplace=True)
-print(df8)
+print("Smallest values for each cytokine:")
+print(smallest_values)
+print("\nLargest values for each cytokine:")
+print(largest_values)
 
-# define cytokines
+#create arrays
 cytokines = ['il8', 'il1', 'il6', 'il10', 'tnf', 'tgf']
 
+# Remove brackets and convert to float
+for col in cytokines:
+    data[col] = data[col].str.strip('[]').astype(float)
+
 # get unique time values
-unique_time = df8['time'].unique()
+unique_time = data['time'].unique()
 
 arrays = {}
 
 # iterate over unique time values
 for time in unique_time:
     # filter data for current value of time
-    df_time = df8[df8['time'] == time]
+    data_time = data[data['time'] == time]
     
-    # initialize 500x500 array for current value of time
-    array = np.zeros((500, 500, len(cytokines)))
+    # initialize 50x50x6 array for current value of time
+    array = np.zeros((50, 50, len(cytokines)))
     
-    # iterate over rows in filtered df
-    for index, row in df_time.iterrows():
-        # get X and Y coordinates
-        x = int(row['xCOM'])
-        y = int(row['yCOM'])
-        
-        # get cytokine concentrations
-        concentrations = row[['il8', 'il1', 'il6', 'il10', 'tnf', 'tgf']].values
-        
-        # assign cytokine concentrations to corresponding position in array
-        array[x, y] = concentrations
+    # get X and Y coordinates
+    x = data_time['xCOM'].astype(int)
+    y = data_time['yCOM'].astype(int)
+    
+    # get cytokine concentrations
+    concentrations = data_time[['il8', 'il1', 'il6', 'il10', 'tnf', 'tgf']].values
+    
+    # assign cytokine concentrations to corresponding position in array
+    array[x, y, :] = concentrations
     
     # store array for current value of time
     arrays[time] = array
 
+
+#should print:
+#Number of arrays: 101
+#Shape of the array: (50, 50, 6)
+#Value at position (18,35): [3.7056725e-07 0.0000000e+00 0.0000000e+00 0.0000000e+00 0.0000000e+00
+# 0.0000000e+00]
+
+print("Number of arrays:", len(arrays))
+array = arrays[0]
+print("Shape of the array:", array.shape)
+print("Value at position (18,35):", array[18,35])
+
+
+
+
+#create input / output pairs
 
 sequence_length = 10
 input_sequences = []
@@ -87,6 +121,18 @@ for i in range(len(arrays_np) - sequence_length):
 input_sequences = np.array(input_sequences)
 output_values = np.array(output_values)
 
+#should print:
+#(91, 10, 50, 50, 6)
+#(91, 50, 50, 6)
+
+print(input_sequences.shape)
+print(output_values.shape)
+
+
+
+# metrics
+
+# define metrics, lr scheduler
 def rmse(y_true, y_pred):
     return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
@@ -94,14 +140,6 @@ def r_squared(y_true, y_pred):
     SS_res =  K.sum(K.square(y_true - y_pred)) 
     SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
     return 1 - SS_res/(SS_tot + K.epsilon())
-
-def lr_schedule(epoch, lr):
-    if epoch < 100:
-        return 1e-2
-    if epoch < 500:
-        return 1e-3
-    else:
-        return 1e-4
 
 def average_relative_rmse(y_true, y_pred):
     return K.sqrt(K.mean(K.square((y_pred - y_true) / K.clip(K.abs(y_true), K.epsilon(), None))))
@@ -111,64 +149,120 @@ def average_relative_rmse(y_true, y_pred):
 
 def accuracy(y_true, y_pred):
     abs_diff = K.abs(y_true - y_pred)
-    threshold = 0.3 * y_true
+    threshold = 0.2 * y_true
     accurate_predictions = K.less_equal(abs_diff, threshold)
     accuracy = K.mean(accurate_predictions)
     return accuracy
 
-#def lr_range_test(epoch, lr):
-    # Start with a very small learning rate and increase exponentially
-    return 10 ** (epoch / 20)  # Adjust the exponent as needed
-
 #def explained_variance(y_true, y_pred):
     return 1 - K.var(y_true - y_pred) / K.var(y_true)
 
+
+def lr_schedule(epoch, lr):
+    if epoch < 100:
+        return 1e-3
+    if epoch < 400:
+        return 1e-4
+    else:
+        return 1e-5
+
+
+# train split
+train_size = int(0.7 * len(input_sequences))
+val_size = int(0.1 * len(input_sequences))
+
+X_train, y_train = input_sequences[:train_size], output_values[:train_size]
+X_val, y_val = input_sequences[train_size:train_size+val_size], output_values[train_size:train_size+val_size]
+X_test, y_test = input_sequences[train_size+val_size:], output_values[train_size+val_size:]
+
+
+
+#model
+def build_model(input_shape):
+    input_layer = layers.Input(shape=input_shape)
+    
+    
+    x = layers.TimeDistributed(layers.Conv2D(64, (3, 3), padding='same', activation='relu'))(input_layer)
+    x = layers.TimeDistributed(layers.MaxPooling2D((2, 2)))(x)
+    x = layers.TimeDistributed(layers.Conv2D(128, (3, 3), padding='same', activation='relu'))(x)
+    x = layers.TimeDistributed(layers.MaxPooling2D((2, 2)))(x)
+    
+    x = layers.Reshape((input_shape[0], -1))(x)
+    x = layers.LSTM(128, return_sequences=True,kernel_regularizer=l2(0.01))(x)
+    x = layers.LSTM(64)(x)
+    
+    x = layers.Dense(50 * 50 * 6, activation='relu')(x)
+    output_layer = layers.Reshape((50, 50, 6))(x)
+    
+    model = models.Model(inputs=input_layer, outputs=output_layer)
+    
+    return model
+
+input_shape = (sequence_length, 50, 50, 6)
+model = build_model(input_shape)
 lr_scheduler = LearningRateScheduler(lr_schedule)
 early_stopping = EarlyStopping(monitor='val_loss', patience=150, verbose=1, restore_best_weights=True)
 
-initial_lr = 1e-2
-sequence_length =10
+initial_lr = 1e-3
 
-input_sequences_reshaped = input_sequences.reshape(input_sequences.shape[0], input_sequences.shape[1], -1)
+model.compile(optimizer='adam', loss='mse', metrics=[r_squared, 'mape', 'accuracy', average_relative_rmse, 'msle', 'mae'])
 
-train_size = int(0.7 * input_sequences_reshaped.shape[0])
-val_size = int(0.1 * input_sequences_reshaped.shape[0])
-test_size = input_sequences_reshaped.shape[0] - train_size - val_size
+history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=500, batch_size=16, callbacks=[lr_scheduler, early_stopping])
 
-X_train = input_sequences_reshaped[:train_size]
-X_val = input_sequences_reshaped[train_size:train_size + val_size]
-X_test = input_sequences_reshaped[train_size + val_size:]
-y_train = output_values[:train_size]
-y_val = output_values[train_size:train_size + val_size]
-y_test = output_values[train_size + val_size:]
-
-print("X_train shape:", X_train.shape)
-print("X_val shape:", X_val.shape)
-print("X_test shape:", X_test.shape)
-print("y_train shape:", y_train.shape)
-print("y_val shape:", y_val.shape)
-print("y_test shape:", y_test.shape)
-
-model = Sequential()
-model.add(LSTM(units=256, return_sequences=True, input_shape=(sequence_length, 50 * 50 * 6), kernel_regularizer=l2(0.03)))
-#model.add(Dropout(0.2)) 
-model.add(LSTM(units=256, return_sequences=True, input_shape=(sequence_length, 50 * 50 * 6), kernel_regularizer=l2(0.03)))
-#model.add(Dropout(0.2)) 
-model.add(LSTM(units=256))
-#model.add(Dropout(0.2)) 
-model.add(Dense(units=256, kernel_regularizer=l2(0.03)))
-#model.add(Dropout(0.2)) 
-model.add(Dense(units=256, kernel_regularizer=l2(0.03)))
-#model.add(Dropout(0.2)) 
-model.add(Dense(units=256, kernel_regularizer=l2(0.03)))
-model.add(Dense(units=50 * 50 * 6, activation='linear'))
-model.add(Reshape((50, 50, 6)))
-model.compile(optimizer=Adam(learning_rate=initial_lr), loss='mse', metrics=[r_squared, 'mape', accuracy, average_relative_rmse, 'msle', 'mae'])
-print(model.summary())
+test_metrics = model.evaluate(X_test, y_test)
+print(f'Test Metrics: {test_metrics}')
 
 
-history = model.fit(X_train, y_train, epochs=1500, batch_size=100, validation_data=(X_val, y_val), callbacks=[lr_scheduler, early_stopping])
-print("Training Loss:", history.history['loss'])
 
-loss = model.evaluate(X_test, y_test)
-print("Test Loss:", loss)
+#export data
+
+y_pred = model.predict(X_test)
+
+y_pred_shape = y_pred.shape
+y_test_shape = y_test.shape
+
+# Flatten the y_pred and y_test tensors
+y_pred_flattened = np.reshape(y_pred, (y_pred_shape[0], -1, y_pred_shape[-1]))
+y_test_flattened = np.reshape(y_test, (y_test_shape[0], -1, y_test_shape[-1]))
+
+# Create arrays for x and y coordinates
+X = np.repeat(np.arange(y_test_shape[1]), y_test_shape[2])
+Y = np.tile(np.arange(y_test_shape[2]), y_test_shape[1])
+
+# Initialize a list to hold all data for the DataFrame
+all_data = []
+
+# Loop through each timestep and collect the data
+for timestep in range(y_pred_shape[0]):
+    y_pred_timestep = y_pred_flattened[timestep]
+    for i in range(y_pred_timestep.shape[0]):
+        data_point = {'timestep': timestep, 'X': X[i], 'Y': Y[i]}
+        data_point.update({f'feature_{j+1}': y_pred_timestep[i, j] for j in range(y_pred_shape[-1])})
+        all_data.append(data_point)
+
+# Create the DataFrame
+df_all_timesteps = pd.DataFrame(all_data)
+
+# Save the DataFrame to a CSV file
+df_all_timesteps.to_csv('data/test-LSTM(82-100hrs).csv', index=False)
+
+#plot loss curve
+
+train_loss = history.history['loss'][1:1000]
+val_loss = history.history['val_loss'][1:1000]
+
+output_dir = 'plots-test'
+os.makedirs(output_dir, exist_ok=True)
+
+plt.plot(train_loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.yscale('log')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training and Validation Loss')
+plt.legend()
+plt.xlim(1, 1000)  # Force the x-axis scale to be from 1 to 1500
+plot_filename = f'LSTM_loss.png'
+plot_path = os.path.join(output_dir, plot_filename)
+plt.savefig(plot_path)
+plt.show()
